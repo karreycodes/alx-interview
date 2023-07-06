6190c0449646d62dd63b651a999b88c8aee87182
Wangari <109669003+karreycodes@users.noreply.github.com>
2023-07-06 20:52:42 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,23 +2,19 @@
 '''returns true of locboxes can all be opened and false if not'''
 
 
-def join(lst1, lst2):
-    results = []
-    for num in lst2:
-        results += lst1[num]
-    return results
-
-
 def canUnlockAll(boxes):
-    '''returns true of locboxes can all be opened and false if not'''
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
-        return len(total) == len(boxes)
+    num_boxes = len(boxes)
+    unlocked_boxes = [False] * num_boxes
+    unlocked_boxes[0] = True
+
+    stack = [0]  # Start with the first box
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key < num_boxes and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                stack.append(key)
+
+    return all(unlocked_boxes)
